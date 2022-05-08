f3cff92eb66de4d2a422f85f02027d1e3d861ea1
LiJinshuai <1871969906@qq.com>
2022-05-08 16:36:45 +0800

    初始Demo：仅包含widget的Mainwindows

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,27 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    ui->Sys_Time->setDigitCount(15);
-    this->startTimer(1000);
 
-
-
-    QToolBar *FileToolbar = addToolBar(tr("文件"));
-    FileToolbar->addAction(ui->action_New);
-    FileToolbar->addAction(ui->action_Open);
-    FileToolbar->addAction(ui->action_Save);
-    FileToolbar->addWidget(ui->Sys_Time);
-
-
-  //  connect(ui->Bt_1,SIGNAL(clicked(bool)),this,SLOT(on_Bt_1_clicked()));
-
-   // QPushButton *Bt_1 = new QPushButton;
-  //  connect(Bt_1,   SIGNAL(clicked())   ,this,  SLOT(closed()));
-    /*      Qt5中兼容中文的头文件,Qt已自动兼容 */
-    //QTextCodec::setCodecForCStrings(QTextCodec::codecForName("GB2312"));
-
-    this->setWindowTitle("我修改了窗口");
-    //this->setCentralWidget(ui->menubar);
 }
 
 MainWindow::~MainWindow()
@@ -39,33 +19,9 @@ MainWindow::~MainWindow()
 }
 void MainWindow::about()
 {
-   // QMessageBox::about(this, "消息","选择了菜单项");
+
 }
-/*//  void MainWindow::on_action_O_triggered()
-//  {
 
-//  }
 
-//  void MainWindow::on_action_E_triggered()
-//  {
 
-//  }
-//  void MainWindow::on_action_S_triggered()
-//  {
 
-//  }*/
-
-void MainWindow::on_Bt_1_clicked()
-
-{
-    QMessageBox::information(this,"提示","要关闭了");
-    this->close();
-}
-void MainWindow::timerEvent(QTimerEvent *event)
-{
-
-    QTime C_time = QTime::currentTime();
-
-    //ui->Sys_Time->setDigitCount(15)
-    ui->Sys_Time->display(C_time.toString("HH:mm:ss . zzz"));
-}
