e2987651fb8f3b8ab2d1016b4a251184cb49014d
LiJinshuai <1871969906@qq.com>
2022-06-04 17:11:01 +0800

    创建Socket客户端，成功建立连接，断开连接

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    TcpClient_ToPLC = new QTcpSocket;
 
 
 }
@@ -15,8 +16,38 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+   // delete TcpClient_ToPLC;
 }
 
 
 
+
+
+void MainWindow::on_Connect_pushButton_clicked()
+{
+    PLC_Ip = ui->IP_lineEdit->text();
+    PLC_Port = ui->Port_lineEdit->text();
+
+    qDebug() << PLC_Ip << PLC_Port;
+
+    TcpClient_ToPLC->connectToHost(QHostAddress(PLC_Ip),PLC_Port.toShort());
+    connect(TcpClient_ToPLC,&QTcpSocket::connected,[this](){
+        ui->Connect_pushButton->setText("已连接");
+        QMessageBox::information(this,"连接提示","连接成功");
+    });
+    connect(TcpClient_ToPLC,&QTcpSocket::disconnected,[this](){
+        ui->Connect_pushButton->setText("连接失败");
+        QMessageBox::warning (this,"连接提示","连接失败");
+    });
+   // if (TcpClient_ToPLC->connected())
+
+
+
+}
+
+
+void MainWindow::on_DisConnect_pushButton_clicked()
+{
+    TcpClient_ToPLC->disconnectFromHost();
+}
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
+#include <QString>
+#include <QDebug>
+#include <QMessageBox>
 
 
 QT_BEGIN_NAMESPACE
@@ -15,11 +19,19 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+     QString PLC_Ip,PLC_Port;          //声明Ip和端口变量
 
 private slots:
 
+    // void on_Connect_pushButton_clicked(bool checked);
+
+     void on_Connect_pushButton_clicked();
+
+     void on_DisConnect_pushButton_clicked();
+
 private:
    Ui::MainWindow *ui;
+   QTcpSocket *TcpClient_ToPLC;     //创建客户端
 
 };
 #endif // MAINWINDOW_H
